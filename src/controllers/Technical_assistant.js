8e9654af0a3ff09dc72ecee1993f56ad9fe887b9
LoreDufa <lorenadufaur@gmail.com>
2024-11-08 13:05:54 -0300

    agrego thread_id

--- src/controllers/Technical_assistant.js
+++ src/controllers/Technical_assistant.js
@@ -1,5 +1,4 @@
 const OpenAI = require('openai');
-const axios = require('axios');
 
 
 const {openai_org, openai_api_key, id_assistant_ta} = require('../utils/config/index')
@@ -15,15 +14,23 @@ const openai = new OpenAI({
     organization: organization,
 })
 
-
 const Technical_assistant = async (req, res) => {
     try {
         const query = req.body.query;
+        const existingThreadId = req.body.thread_id; // Verifica si `thread_id` existe en el body
         console.log('Consulta del usuario:', query);
 
-        const thread = await openai.beta.threads.create(); // Crea un hilo de conversación
-        const thread_id = thread.id; // Asigna el nuevo thread_id
-        
+        let thread_id;
+
+        if (existingThreadId) {
+            // Si `thread_id` está en el body, úsalo
+            thread_id = existingThreadId;
+        } else {
+            // Si no existe, crea un nuevo hilo
+            const thread = await openai.beta.threads.create();
+            thread_id = thread.id;
+        }
+
         // Modificar el mensaje para aclarar el alcance de la búsqueda
         const messageText = `Responde utilizando el documento FortiOS-7.6.0-Administration_Guide. Pregunta del usuario: ${query}`;
 
@@ -34,28 +41,27 @@ const Technical_assistant = async (req, res) => {
 
         const run = await openai.beta.threads.runs.create(thread_id, {
             assistant_id: assistant_id,
-            
         });
         const run_id = run.id;
 
         let retries = 10;
         const interval = 2000;
         let responseSent = false;
-            
+
         while (retries > 0 && !responseSent) {
             const runs = await openai.beta.threads.runs.retrieve(thread_id, run_id);
             console.log("Estado actual: ", runs.status);
-            
+
             if (runs.status === "completed") {
                 const messages = await openai.beta.threads.messages.list(thread_id);
                 const response = messages.data[0].content[0].text.value;
-                
+
                 if (response) {
-                    res.status(200).send(response);
+                    res.status(200).send({ response, thread_id});
                 } else {
                     res.status(400).send({ message: 'No hay respuesta relevante en los documentos.' });
                 }
-                
+
                 responseSent = true;
             } else if (runs.status === "failed") {
                 res.status(500).send({ message: 'Error en la ejecución.' });
